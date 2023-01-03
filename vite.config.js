d65f672ff13436b970fed35019e3cb649870d9ef
LarsG <lars.ga.solheim@gmail.com>
2023-01-03 14:48:27 +0100

    fixed base url v2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/testing-testing-gh-pages/"
+  base: "/testing-testing-vite-gh-pages/"
 })
